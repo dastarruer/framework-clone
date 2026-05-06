6ff8c1276350ce83b9027c7ecb5b6121908af173
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-06 14:28:10 -0400

    Init Hero component w Background

---
+++ src/lib/components/hero/Background.svelte
@@ -0,0 +1,11 @@
+<div class="hero">
+	<svg viewBox="0 0 1440 600" preserveAspectRatio="none">
+		<path d="M0,100 Q720,300 1440,100 L1440,600 L0,600 Z" fill="#ff6b6b" />
+		<path d="M0,200 Q720,400 1440,200 L1440,600 L0,600 Z" fill="#ffd93d" />
+		<!-- more strips -->
+	</svg>
+	<div class="content">
+		<h1>Laptop Name</h1>
+		<button>Configure Now</button>
+	</div>
+</div>
---
+++ src/lib/components/hero/Hero.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Background from '$lib/components/hero/Background.svelte';
+</script>
+
+<Background />
