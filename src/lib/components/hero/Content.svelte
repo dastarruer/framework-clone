43b765cf05bcd838890ff7e90d4177c3f1d86e33
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-07 14:05:48 -0400

    Add rest of hero content + styling

--- src/lib/components/hero/Content.svelte
+++ src/lib/components/hero/Content.svelte
@@ -1,1 +1,12 @@
-<h1 class="font-sans text-6xl font-bold text-black">Framework Laptop 16</h1>
+<div class="flex w-1/2 flex-col items-center gap-5">
+	<h1 class="font-sans text-6xl font-bold text-black">Framework Laptop 16</h1>
+	<p class="text-2xl">
+		Framework Laptop 13 is a thin, light, and fast 13.5” notebook that is simple to customize,
+		upgrade, and repair, so it stays yours for longer.
+	</p>
+	<button
+		class="pointer-events-auto h-14 w-56 cursor-pointer rounded-4xl bg-orange text-xl font-bold hover:bg-orange-hover active:bg-orange-click"
+	>
+		Configure now
+	</button>
+</div>
