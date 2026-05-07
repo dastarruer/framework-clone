30de5901655c047c11cdef881778a24a52f80ab6
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-07 13:45:22 -0400

    Rename Title component to Content

--- src/lib/components/hero/Hero.svelte
+++ src/lib/components/hero/Hero.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import Background from '$lib/components/hero/Background.svelte';
-	import Title from '$lib/components/hero/Title.svelte';
+	import Content from '$lib/components/hero/Content.svelte';
 </script>
 
 <div class="relative">
 	<Background />
 	<div class="absolute inset-0 flex h-full items-center justify-center">
-		<Title />
+		<Content />
 	</div>
 </div>
