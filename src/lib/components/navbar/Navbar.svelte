70b173ae83225114b1de2655fa7060c16654d027
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-11 14:11:50 -0400

    Add a not-so-usable navbar

---
+++ src/lib/components/navbar/Navbar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { asset } from '$app/paths';
+	import { NavigationMenu } from 'bits-ui';
+
+	type ListItemProps = {
+		img?: string;
+		title: string;
+	};
+</script>
+
+{#snippet ListItem({ img, title }: ListItemProps)}
+	<NavigationMenu.Item>
+		<NavigationMenu.Trigger>{title}</NavigationMenu.Trigger>
+		<NavigationMenu.Content class="z-20 flex w-svw bg-amber-300">
+			<img src={img} alt="Picture of {title}" />
+			<div>
+				<h1>{title}</h1>
+				<p>Learn more</p>
+				<p>Shop parts</p>
+				<p>Downloads and guides</p>
+				<p>Configure now</p>
+			</div>
+		</NavigationMenu.Content>
+	</NavigationMenu.Item>
+{/snippet}
+
+<nav class="nav-wave fixed top-0 left-0 z-100 h-24 w-full bg-slate-900 text-white">
+	<NavigationMenu.Root class="relative">
+		<NavigationMenu.List class="relative flex h-24 items-center px-8">
+			{@render ListItem({
+				img: asset('/desktop.jpg'),
+				title: 'Desktop'
+			})}
+		</NavigationMenu.List>
+	</NavigationMenu.Root>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Hero from '$lib/components/hero/Hero.svelte';
+	import Navbar from '$lib/components/navbar/Navbar.svelte';
 </script>
 
+<Navbar />
 <Hero />
