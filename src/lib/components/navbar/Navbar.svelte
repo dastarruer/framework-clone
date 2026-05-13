b4e5d224534b48ae72d99b36081f57bb69df0b1e
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-13 14:17:55 -0400

    Add elements to navbar, change to regular shape, change bg, remove content

--- src/lib/components/navbar/Navbar.svelte
+++ src/lib/components/navbar/Navbar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { asset } from '$app/paths';
+	// import { asset } from '$app/paths';
 	import { NavigationMenu } from 'bits-ui';
 
 	type ListItemProps = {
@@ -8,29 +8,48 @@
 	};
 </script>
 
-{#snippet ListItem({ img, title }: ListItemProps)}
+<!-- img isn't used right now since content is broken -->
+<!-- {#snippet ListItem({ img, title }: ListItemProps)} -->
+{#snippet ListItem({ title }: ListItemProps)}
 	<NavigationMenu.Item>
-		<NavigationMenu.Trigger>{title}</NavigationMenu.Trigger>
-		<NavigationMenu.Content class="z-20 flex w-svw bg-amber-300">
+		<NavigationMenu.Trigger class="cursor-pointer hover:underline">{title}</NavigationMenu.Trigger>
+		<!-- content is broekn right now -->
+		<!-- <NavigationMenu.Content class="z-20 flex w-svw bg-amber-300">
 			<img src={img} alt="Picture of {title}" />
 			<div>
 				<h1>{title}</h1>
-				<p>Learn more</p>
-				<p>Shop parts</p>
-				<p>Downloads and guides</p>
-				<p>Configure now</p>
+				<p class="cursor-pointer hover:underline">Learn more</p>
+				<p class="cursor-pointer hover:underline">Shop parts</p>
+				<p class="cursor-pointer hover:underline">Downloads and guides</p>
+				<p class="cursor-pointer hover:underline">Configure now</p>
 			</div>
-		</NavigationMenu.Content>
+		</NavigationMenu.Content> -->
 	</NavigationMenu.Item>
 {/snippet}
 
-<nav class="nav-wave fixed top-0 left-0 z-100 h-24 w-full bg-slate-900 text-white">
+<nav
+	class="fixed top-0 left-0 z-100 h-20 w-full border-b border-stone-300 bg-background text-black drop-shadow-lg/10 drop-shadow-background-alt"
+>
 	<NavigationMenu.Root class="relative">
-		<NavigationMenu.List class="relative flex h-24 items-center px-8">
-			{@render ListItem({
-				img: asset('/desktop.jpg'),
-				title: 'Desktop'
-			})}
+		<NavigationMenu.List class="relative flex h-20 items-center px-8">
+			<div class="flex gap-8">
+				{@render ListItem({
+					title: 'Desktop'
+				})}
+				{@render ListItem({
+					title: 'Laptop 12'
+				})}
+				{@render ListItem({
+					title: 'Laptop 13'
+				})}
+				{@render ListItem({
+					title: 'Laptop 16'
+				})}
+			</div>
 		</NavigationMenu.List>
+		<!-- i give up on this, i gotta move on -->
+		<!-- <NavigationMenu.Viewport
+			class="absolute top-full left-0 w-full bg-amber-300 text-slate-900 shadow-xl"
+		/> -->
 	</NavigationMenu.Root>
 </nav>
