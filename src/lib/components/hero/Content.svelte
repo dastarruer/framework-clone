03581474e9c6f6b7dc25ab50ce880307f4e0a432
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-14 13:28:45 -0400

    Use bits ui button element

--- src/lib/components/hero/Content.svelte
+++ src/lib/components/hero/Content.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { Canvas } from '@threlte/core';
 	import Laptop from './Laptop.svelte';
+	import { Button } from 'bits-ui';
 </script>
 
 <div class="flex w-1/2 flex-col items-center gap-5">
@@ -15,15 +16,15 @@
 		upgrade, and repair, so it stays yours for longer.
 	</p>
 	<div class="flex w-svw justify-center gap-10">
-		<button
+		<Button.Root
 			class="pointer-events-auto h-14 w-56 cursor-pointer rounded-4xl bg-orange text-xl font-bold hover:bg-orange-hover active:bg-orange-click"
 		>
 			Configure now
-		</button>
-		<button
+		</Button.Root>
+		<Button.Root
 			class="pointer-events-auto h-14 w-56 cursor-pointer rounded-4xl border-2 border-solid border-slate-950 bg-white text-xl font-bold hover:bg-neutral-200 active:bg-neutral-300"
 		>
 			Learn more
-		</button>
+		</Button.Root>
 	</div>
 </div>
