422adaa2311e006f423612013636a34027782a9a
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-14 13:27:36 -0400

    Add learn more button

--- src/lib/components/hero/Content.svelte
+++ src/lib/components/hero/Content.svelte
@@ -14,9 +14,16 @@
 		Framework Laptop 13 is a thin, light, and fast 13.5” notebook that is simple to customize,
 		upgrade, and repair, so it stays yours for longer.
 	</p>
-	<button
-		class="pointer-events-auto h-14 w-56 cursor-pointer rounded-4xl bg-orange text-xl font-bold hover:bg-orange-hover active:bg-orange-click"
-	>
-		Configure now
-	</button>
+	<div class="flex w-svw justify-center gap-10">
+		<button
+			class="pointer-events-auto h-14 w-56 cursor-pointer rounded-4xl bg-orange text-xl font-bold hover:bg-orange-hover active:bg-orange-click"
+		>
+			Configure now
+		</button>
+		<button
+			class="pointer-events-auto h-14 w-56 cursor-pointer rounded-4xl border-2 border-solid border-slate-950 bg-white text-xl font-bold hover:bg-neutral-200 active:bg-neutral-300"
+		>
+			Learn more
+		</button>
+	</div>
 </div>
